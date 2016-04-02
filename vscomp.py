4125936b05308834f0ae4771e717bd1365220c50
aditya1904 <adityamalu1@gmail.com>
2016-04-02 21:13:55 +0530

    Found a bug. Fix needed

--- vscomp.py
+++ vscomp.py
@@ -8,11 +8,56 @@ available_positions = [0,1,2,3,4,5,6,7,8]
 global priority_positions
 priority_positions = [4,1,3,5,7]
 
+def find_all():
+	newlist1 = []
+	newlist2 = []
+	newlist3 = []
+	newlist4 = []
+	for i in range(len(scoresdict)):
+		if scoresdict[i][1] == 2:
+			for x in scoresdict[i][0]:
+				newlist1.append(x)
+	for i in range(len(scoresdict)):
+		if scoresdict[i][1] == -2:
+			for x in scoresdict[i][0]:
+				newlist2.append(x)
+	for i in range(len(scoresdict)):
+		if scoresdict[i][1] == 1:
+			for x in scoresdict[i][0]:
+				newlist3.append(x)
+	for i in range(len(scoresdict)):
+		if scoresdict[i][1] == -1:
+			for x in scoresdict[i][0]:
+				newlist3.append(x)
+	if len(newlist1) > 0:
+		return newlist1
+	if len(newlist2) > 0:
+		return newlist2
+	if len(newlist3) > 0:
+		return newlist3
+	if len(newlist4) > 0:
+		return newlist4
+
+
 class Player:
 	def __init__(self, Variable):
 		self.variable = Variable
 	def myturn(self, x_ord):
 		board[ x_ord ] = self.variable
+
+
+
+
+def update_duplicate_scores(variable, position, scoreslist):
+	if variable == comp_var:
+		for i in range(len(scoreslist)):
+			if position in scoreslist[i][0]:
+				scoreslist[i][1] = scoreslist[i][1] + 1
+	if variable == player_var:
+		for i in range(len(scoreslist)):
+			if position in scoreslist[i][0]:
+				scoreslist[i][1] = scoreslist[i][1] - 1
+	return scoreslist
 
 def Update_Scores(variable, position):
 	if variable == comp_var:
@@ -24,59 +69,28 @@ def Update_Scores(variable, position):
 			if position in scoresdict[i][0]:
 				scoresdict[i][1] = scoresdict[i][1] - 1
 	
-def Update_Scoredict():
-	for i in range(len(scoresdict)):
-		temp = [ x for x in available_positions if x in scoresdict[i]]
-		if len(temp) == 0:
-			del scoresdict[i]
-def play_Computer():
-	for i in range(len(scoresdict)):
-		if scoresdict[i][1] == 2:
-			print 'Selected COmbo',scoresdict[i][0]
-			print 'Score', scoresdict[i][1]
-			pos_avail = find_pos(scoresdict[i][0])
-			print 'returned position', pos_avail
-			return pos_avail
-	for i in range(len(scoresdict)):
-		if scoresdict[i][1] == -2:
-			print 'Selected COmbo',scoresdict[i][0]
-			print 'Score', scoresdict[i][1]
-			pos_avail = find_pos(scoresdict[i][0])
-			print 'returned position', pos_avail
-			return pos_avail
-	for i in range(len(scoresdict)):
-		if scoresdict[i][1] == 1:
-			print 'Selected COmbo',scoresdict[i][0]
-			print 'Score', scoresdict[i][1]
-			pos_avail = find_pos(scoresdict[i][0])
-			print 'returned position', pos_avail
-			return pos_avail
-	for i in range(len(scoresdict)):
-		if scoresdict[i][1] == -1:
-			print 'Selected COmbo',scoresdict[i][0]
-			print 'Score', scoresdict[i][1]
-			pos_avail = find_pos(scoresdict[i][0])
-			print 'returned position', pos_avail
-			return pos_avail
-	
-	
 def find_pos(templist):
 	temp = [x for x in available_positions if x in templist]
-	print 'Available Positions', temp
-	if len(temp) == 1:
-		return temp[0]
-	temp2 = [x for x in priority_positions if x in temp]
-	print 'Available from priority', temp2
-	if len(temp2) == 0:
-		return temp[0]
-	else:
-		return temp2[0]
+	return temp
 
 def init_scores():
 	global scoresdict
 	scoresdict = {}
 	for i in range(len(winning_positions)):
 		scoresdict[i] = [winning_positions[i],0]
+
+def play_Computer():
+	ret_list = find_all()
+	ret_list = list(set(ret_list))
+	bestlist = find_pos(ret_list)
+	print 'Bestlist' , bestlist
+	pr_list = [x for x in priority_positions if x in bestlist]
+	print 'pr_list', pr_list
+	if len(pr_list) == 0:
+		return bestlist[0]
+	else:
+		return pr_list[0]
+
 
 def Check_Win(variable):
 	win = False
@@ -99,17 +113,16 @@ def main():
 	init_scores()
 	while True:
 		global player_var
-		player_var = 'X'
+		player_var = 'O'
 		global comp_var
-		comp_var = 'O'
-		user = Player('X')
+		comp_var = 'X'
+		user = Player('O')
 		##PLayer PLays##
 		print "Player turn...Input Square No.."
 		player_chose_position = int(raw_input())
 		available_positions.remove(player_chose_position)
 		user.myturn(player_chose_position)
 		Update_Scores(player_var, player_chose_position)
-#		Update_Scoredict()
 		print scoresdict
 		display()
 		if Check_Win(player_var):
@@ -121,7 +134,6 @@ def main():
 		board[comp_chose_position] = comp_var
 		available_positions.remove(comp_chose_position)
 		Update_Scores(comp_var, comp_chose_position)
-#		Update_Scoredict()
 		print scoresdict
 		display()
 		if Check_Win(comp_var):
